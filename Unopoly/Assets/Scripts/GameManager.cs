5b6e838f350483cd540175aa9faa033ef6964175
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-04-27 03:45:52 +0300

    Game Tokens

--- Unopoly/Assets/Scripts/GameManager.cs
+++ Unopoly/Assets/Scripts/GameManager.cs
@@ -10,13 +10,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] MonopolyBoard gameBoard;
     [SerializeField] List<Player> playerList = new List<Player>();
     [SerializeField] int currentPlayer;
+    [Header("Global Game Settings")]
     [SerializeField] int maxTurnsInJail = 3; //how long a player can stay in jail
     [SerializeField] int startingMoney = 1500;
     [SerializeField] int goMoney = 500;
-
+    [Header("Player Info")]
     [SerializeField] GameObject playerInfoPrefab;
     [SerializeField] Transform playerPanel; //for the player info prefabs to become parented to
-
+    [SerializeField] List<GameObject> playerTokenList = new List<GameObject>();
 
 
     private void Awake()
@@ -26,13 +27,24 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        //initialize the players
         for (int i = 0; i < playerList.Count; i++)
         {
             GameObject infoObject = Instantiate(playerInfoPrefab, playerPanel, false);
             PlayerInfo info = infoObject.GetComponent<PlayerInfo>();
-            playerList[i].Init(gameBoard.route[0], startingMoney, info);
+
+            //randomize the player token
+            int randIndex = Random.Range(0, playerTokenList.Count);
+            //initialize the player
+            GameObject newToken = Instantiate(playerTokenList[randIndex], gameBoard.route[0].transform.position, Quaternion.identity);
+
+            playerList[i].Init(gameBoard.route[0], startingMoney, info, newToken);
         }
     }
-
 
 }
